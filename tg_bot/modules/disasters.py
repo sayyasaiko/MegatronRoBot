e0fea3c5cfde1566a59b6a7412dc5649b2976dd6
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2020-05-05 22:28:11 +0530

    Update disasters.py

--- tg_bot/modules/disasters.py
+++ tg_bot/modules/disasters.py
@@ -28,16 +28,16 @@ def check_user_id(user_id: int, bot: Bot) -> Optional[str]:
     return reply
 
 #I added extra new lines 
-Nations = """ Kigyō has bot access levels we call as *"Nation Levels"*
+Nations = """ Megatron has bot access levels we call as *"Nation Levels"*
 \n*Eagle Union* - Devs who can access the bots server and can execute, edit, modify bot code. Can also manage other Nations
 \n*God* - Only one exists, bot owner. 
-Owner has complete bot access, including bot adminship in chats Kigyō is at.
-\n*Royals* - Have super user access, can gban, manage Nations lower than them and are admins in Kigyō.
-\n*Sakuras* - Have access go globally ban users across Kigyō.
+Owner has complete bot access, including bot adminship in chats Megatron is at.
+\n*Royals* - Have super user access, can gban, manage Nations lower than them and are admins in Megatron.
+\n*Sakuras* - Have access go globally ban users across Megatron.
 \n*Sardegnas* - Same as Neptunians but can unban themselves if banned.
 \n*Neptunians* - Cannot be banned, muted flood kicked but can be manually banned by admins.
-\n*Disclaimer*: The Nation levels in Kigyō are there for troubleshooting, support, banning potential scammers.
-Report abuse or ask us more on these at [Eagle Union](https://t.me/YorktownEagleUnion).
+\n*Disclaimer*: The Nation levels in Megatron are there for troubleshooting, support, banning potential scammers.
+Report abuse or ask us more on these at [ Megatron Support](https://t.me/MegatronSupportGroup).
 """
 # do not async, not a handler 
 def send_Nations(update):
@@ -559,7 +559,7 @@ __help__ = """
  - /Sardegnas - Lists all Sardegnas Nations.
  - /Neptunians - Lists all Neptunia Nations.
  Note: These commands list users with special bot priveleges and can only be used by them.
- You can visit @YorktownEagleUnion to query more about these.
+ You can visit @MegatronSupportGroup to query more about these.
 """
 
 SUDO_HANDLER = CommandHandler(("addsudo", "addRoyal"), addsudo, pass_args=True)
