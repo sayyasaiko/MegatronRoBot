567a359c60447d6fbad7ecebf58627c360a09bcd
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2020-05-11 22:52:19 +0530

    Update urlblacklist.py

--- tg_bot/modules/urlblacklist.py
+++ tg_bot/modules/urlblacklist.py
@@ -158,22 +158,8 @@ URL_DELETE_HANDLER = MessageHandler(
     edited_updates=True)
 
 
-__mod_name__ = "Domain Blacklists"
 
-__help__ = """
-Domain blacklisting is used to stop certain domains from being mentioned in a group, Any time an url on that domain is mentioned, /
-the message will immediately be deleted.
-
-*NOTE:* domain blacklisting do not affect group admins.
-
-- /geturl: View the current blacklisted urls
-
-*Admin only:*
-
-- /addurl <urls>: Add a domain to the blacklist. The bot will automatically parse the url.
-- /delurl <urls>: Remove urls from the blacklist
-
-"""
+__help__ = False
 
 dispatcher.add_handler(URL_BLACKLIST_HANDLER)
 dispatcher.add_handler(ADD_URL_BLACKLIST_HANDLER)
