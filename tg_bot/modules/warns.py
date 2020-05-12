518055a76d6ff1e2f76948d0b8553f9df7d06650
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2020-05-12 22:17:01 +0530

    Update warns.py

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -442,20 +442,8 @@ def __migrate__(old_chat_id, new_chat_id):
     sql.migrate_chat(old_chat_id, new_chat_id)
 
 
-__mod_name__ = "Warnings"
     
-__help__help__ = """
- - /warns <userhandle>: get a user's number, and reason, of warns.
- - /warnlist: list of all current warning filters
-*Admins only:*
- - /warn <userhandle>: warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
- - /resetwarn <userhandle>: reset the warns for a user. Can also be used as a reply.
- - /addwarn <keyword> <reply message>: set a warning filter on a certain keyword. If you want your keyword to \
-be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is an angry user`. 
- - /nowarn <keyword>: stop a warning filter
- - /warnlimit <num>: set the warning limit
- - /strongwarn <on/yes/off/no>: If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
-"""
+__help__help__ = False
 
 
 WARN_HANDLER = DisableAbleCommandHandler("warn",
