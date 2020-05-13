404b46394a228bfcd46b7bfebde975e33e874657
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2020-05-13 15:40:55 +0530

    Update Limits.py

--- tg_bot/modules/Limits.py
+++ tg_bot/modules/Limits.py
@@ -32,7 +32,7 @@ def send_rules(update, chat_id, from_pm=False):
         else:
             raise
 
-    rules = sql.get_rules(chat_id)
+    limits = sql.get_limits(chat_id)
     text = f"The rules for *{escape_markdown(chat.title)}* are:\n\n{rules}"
 
     if from_pm and limits:
