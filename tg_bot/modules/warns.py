0e833de62495f262f0b3459c810e5eb357c4aca5
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2020-05-13 16:03:14 +0530

    Update warns.py

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -442,7 +442,18 @@ def __migrate__(old_chat_id, new_chat_id):
     sql.migrate_chat(old_chat_id, new_chat_id)
 
 
-__help__ = True
+__help__ = """
+- /warns <userhandle>: get a user's number, and reason, of warns.
+ - /warnlist: list of all current warning filters
+
+Admins only:
+ - /warn <userhandle>: warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
+ - /resetwarn <userhandle>: reset the warns for a user. Can also be used as a reply.
+ - /addwarn <keyword> <reply message>: set a warning filter on a certain keyword. If you want your keyword to be a sentence, encompass it with quotes, as such: /addwarn "very angry" This is an angry user. 
+ - /nowarn <keyword>: stop a warning filter
+ - /warnlimit <num>: set the warning limit
+ - /strongwarn <on/yes/off/no>: If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
+"""
 
 WARN_HANDLER = DisableAbleCommandHandler("warn",
                                          warn_user,
@@ -488,6 +499,8 @@ REMOVE_WARNS_HANDLER = CommandHandler(["rmwarn", "unwarn"],
                                       remove_warns,
                                       pass_args=True,
                                       filters=Filters.group)
+
+__mod_name__ = "Warnings"
 
 dispatcher.add_handler(WARN_HANDLER)
 dispatcher.add_handler(RMWARN_QUERY_HANDLER)
